39aec720c9b04d7db023823163cae25e51673d6e
Brian Min <bymi15@yahoo.com>
2021-01-03 11:13:55 +0000

    feat(eventrooms): connect with eventrooms cosmosdb collection

--- eventrooms/index.js
+++ eventrooms/index.js
@@ -1,13 +1,37 @@
+const mongoClient = require("mongodb").MongoClient;
+
+let db = null;
+const loadDatabase = async () => {
+  if (db) return db;
+  const client = await mongoClient.connect(
+    `mongodb://${process.env.DB_USER}.mongo.cosmos.azure.com:${process.env.DB_PORT}/?ssl=true`,
+    {
+      auth: {
+        user: process.env.DB_USER,
+        password: process.env.DB_PASSWORD,
+      },
+    }
+  );
+  db = client.db("uniworlddb");
+  return db;
+};
+
 module.exports = async function (context, req) {
-    context.log('JavaScript HTTP trigger function processed a request.');
+  context.log("JavaScript HTTP trigger function processed a request.");
 
-    const name = (req.query.name || (req.body && req.body.name));
-    const responseMessage = name
-        ? "Hello, " + name + ". This HTTP triggered function executed successfully."
-        : "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.";
+  //   const name = req.query.name || (req.body && req.body.name);
 
+  try {
+    const db = await loadDatabase();
+    let eventRooms = await db.collection("EventRooms").find().toArray();
     context.res = {
-        // status: 200, /* Defaults to 200 */
-        body: responseMessage
+      body: { eventRooms },
     };
-}
+  } catch (err) {
+    context.log(`Error code: ${err.code}, message: ${err.message}`);
+    context.res = {
+      status: 500,
+      body: { message: "An error has occured. Please try again." },
+    };
+  }
+};
